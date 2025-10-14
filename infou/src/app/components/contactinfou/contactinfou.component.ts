ebfb9426a5ef3dbc29827abcbefa94cb3a618687
vamsi7997 <113518353+vamsi7874@users.noreply.github.com>
2025-10-14 13:03:43 +0530

    Added api from front end
    Schedule skeleton functionality done

--- infou/src/app/components/contactinfou/contactinfou.component.ts
+++ infou/src/app/components/contactinfou/contactinfou.component.ts
@@ -1,21 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-contactinfou',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './contactinfou.component.html',
   styleUrl: './contactinfou.component.css'
 })
 export class ContactinfouComponent {
 
+  http = inject(HttpClient);
+
   constructor(){
 
   }
 
-  schedulerOpts = signal([ {name : "Weather",icon : '<i class="fa-solid fa-cloud"></i>'} , {name : "Finanace",icon : '<i class="fa-solid fa-coins"></i>'},
- {name : "Disaster Funds",icon:""},{name : "Horti Cultural Crops" ,icon : ""} ])
+  schedulerOpts = signal([ {name : "Weather",key : "weather",icon : '<i class="fa-solid fa-cloud"></i>'} , {name : "Finanace",key : "finance",icon : '<i class="fa-solid fa-coins"></i>'},
+ {name : "Disaster Funds",key : "disasterfunds",icon:'<i class="fa-solid fa-wheat-awn-circle-exclamation"></i>'},{name : "Horti Cultural Crops" ,key : "horticrops",icon : '<i class="fa-solid fa-building-wheat"></i>'} ])
 
  
+ onClickScheduler(key : string | undefined){
 
+  const uri = 'http://localhost:3000/app/commCall'
+  const payload = {
+    key : key,
+    methodName : "datajobs-startScheduler"
+  }
+
+  this.http.post(uri,payload).subscribe((res)=>{
+    window.alert("Scheduled Successfully..");
+  })
+
+} 
 }
--- infou/src/environment.ts
+++ infou/src/environment.ts
@@ -1,7 +1,7 @@
 export const  environtment = {
     version : "1.0.0",
 
-    signUpUri : "http://localhost:5000",
+    signUpUri : "http://localhost:3000",
     path : "/"
 
 }
